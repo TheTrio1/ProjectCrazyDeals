ebbe2e7104e33b9cffd2b9d144c3256223e0e1ce
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2022-09-02 19:04:49 +0530

    correction in scripts

--- Backend/runserver.sh
+++ Backend/runserver.sh
@@ -3,5 +3,5 @@
 my_app_path=$( dirname $(readlink -f "$0"))
 source "$my_app_path/pyenv/Scripts/activate"
 
-cd "$my_app_path/HospitalManagement"
+cd "$my_app_path/ShoppingCart"
 python manage.py runserver 
