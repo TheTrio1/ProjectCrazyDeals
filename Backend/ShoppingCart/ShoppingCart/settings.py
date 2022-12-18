e72b1ed250f2c99bb8fd1961af3009b04475c93b
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2022-12-18 17:51:29 +0530

    Update

--- Backend/ShoppingCart/ShoppingCart/settings.py
+++ Backend/ShoppingCart/ShoppingCart/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from find_ip import get_current_ip
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x79@4fz5tlj**wgkez)z5$^nrnj0764^wm%sesjcyd&!y5-z56
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [get_current_ip(), "localhost"]
 
 
 # Application definition
